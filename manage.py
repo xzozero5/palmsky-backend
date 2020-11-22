aa7cc0cfe4145f4c796b873021b2acedc26b8cd4
xzozero5 <thagoon.thongpe@mail.kmutt.ac.th>
2020-11-22 18:07:46 +0700

    host

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    """Run administrative tasks."""
+    """Run administrtive tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'palmsky.settings')
     try:
         from django.core.management import execute_from_command_line
